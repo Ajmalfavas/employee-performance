4f3202d2082d207f9c387c0f5baa4063098ab3ef
Ajmalfavas <favuf388@gmail.com>
2025-10-28 21:30:24 +0530

    Router added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard'
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard)
+    }
+    // {
+    //     path: 'dashboard',
+    //     loadComponent: () => import('./features/dashboard/dashboard.component').then((m) => m.DashboardComponent)
+    // },
+    // {
+    //     path: 'reports',
+    //     loadComponent: () => import('./features/reports/reports.component').then((m) => m.ReportsComponent)
+    // },
+    // {
+    //     path: 'settings',
+    //     loadComponent: () => import('./features/settings/settings.component').then((m) => m.SettingsComponent)
+    // }
+];
